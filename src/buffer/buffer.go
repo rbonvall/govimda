8ee761d75fecb4e8acb51853bc860fd63a6b1134
Roberto Bonvallet <rbonvall@gmail.com>
2013-05-21 17:49:33 -0400

    Functions for getting lines by index or number
    These functions introduce a convention for refering to lines:
    a line *index* starts from 0; a line *number* starts from 1 and can be
    negative for counting lines from the bottom of the file.  Indices are
    meant for programmers, while numbers are meant for users.

--- src/buffer/buffer.go
+++ src/buffer/buffer.go
@@ -58,3 +58,42 @@ func NewFromFile(fileName string) (buffer *Type, err error) {
 
 	return
 }
+
+// LineByIndex takes a zero-based line index
+// and returns the corresponding line.
+// Index 0 refers to the last line of the buffer.
+// If the line does not exist, it returns nil.
+func (b *Type) LineByIndex(index int) (line *Line) {
+	line = b.first_line
+	for i := 0; i < index; i++ {
+		line = line.next
+	}
+	return
+}
+
+// LineByIndexInReverse takes a line index
+// and returns the corresponding line in reverse order.
+// Index 0 refers to the last line of the buffer.
+// If the line does not exist, it returns nil.
+func (b *Type) LineByIndexInReverse(index int) (line *Line) {
+	line = b.last_line
+	for i := 0; i < index; i++ {
+		line = line.prev
+	}
+	return
+}
+
+// LineByNumber takes a line number and returns the corresponding line.
+// Negative indices count starting from the last line.
+// Number 1 refers to the first line, number -1 refers to the last line.
+// There is no line 0.
+// If the line does not exist, it returns nil.
+func (b *Type) LineByNumber(nrLine int) (line *Line) {
+	switch {
+	case nrLine > 0:
+		line = b.LineByIndex(nrLine - 1)
+	case nrLine < 0:
+		line = b.LineByIndexInReverse(-nrLine - 1)
+	}
+	return
+}
--- src/buffer/buffer_test.go
+++ src/buffer/buffer_test.go
@@ -58,3 +58,67 @@ func TestNewFromFile(test *testing.T) {
 		current_line = current_line.prev
 	}
 }
+
+func TestLineByIndex(test *testing.T) {
+	name := createTestFile()
+	defer os.Remove(name)
+	buffer, _ := NewFromFile(name)
+
+	for i := 0; i < len(lines); i++ {
+		expected := lines[i]
+		got := buffer.LineByIndex(i).data
+		if expected != got {
+			test.Errorf(`Line index %v. Expected: "%v". Got: "%v"`, i, expected, got)
+		}
+	}
+}
+
+func TestLineByIndexInReverse(test *testing.T) {
+	name := createTestFile()
+	defer os.Remove(name)
+	buffer, _ := NewFromFile(name)
+
+	for i := 0; i < len(lines); i++ {
+		expected := lines[len(lines) - 1 - i]
+		got := buffer.LineByIndexInReverse(i).data
+		if expected != got {
+			test.Errorf(`Line index %v. Expected: "%v". Got: "%v"`, i, expected, got)
+		}
+	}
+}
+
+func TestLineByNumber(test *testing.T) {
+	name := createTestFile()
+	defer os.Remove(name)
+	buffer, _ := NewFromFile(name)
+
+	nrToLine := map[int]*string {
+		-5: nil,
+		-3: &lines[1],
+		-1: &lines[3],
+		 0: nil,
+		 1: &lines[0],
+		 3: &lines[2],
+		 5: nil,
+	}
+	for n, v := range nrToLine {
+		line := buffer.LineByNumber(n)
+		switch line {
+		case nil:
+			if v != nil {
+				expected := *v
+				test.Errorf(`Line number %v. Expected: "%v". Got: nil`, n, expected)
+			}
+		default:
+			got := line.data
+			if v != nil {
+				expected := *v
+				if expected != got {
+					test.Errorf(`Line number %v. Expected: "%v". Got: "%v"`, n, expected, got)
+				}
+			} else {
+				test.Errorf(`Line number %v. Expected: nil. Got: "%v"`, n, got)
+			}
+		}
+	}
+}
